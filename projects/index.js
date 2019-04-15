4be2f0b957acb1aed0d331439b2315d9789abd05
강신광 <shine1115@naver.com>
2019-04-15 15:28:04 +0900

    1-8 예제

--- projects/index.js
+++ projects/index.js
@@ -1,41 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 
 const Links = () => (
     <nav>
         <div><Link to="/">Home</Link></div>
-        <div><Link to="/menu">Menu</Link></div>
+        <div><Link to="/old">Old</Link></div>
+        <div><Link to="/new">New</Link></div>
     </nav>
 );
-
-const Home = () => <h1>Home</h1>;
-const Food = () => <div>food</div>;
-const Drinks = () => <div>drinks</div>;
-const Sides = () => <div>sides</div>;
-const Menu = () => (
-    <div>
-        <div><h1>Menu</h1></div>
-        <div>
-            <nav>
-                <div><Link to="/menu/food">food</Link></div>
-                <div><Link to="/menu/drinks">drinks</Link></div>
-                <div><Link to="/menu/sides">sides</Link></div>
-            </nav>
-        </div>
-    </div>
-)
 
 const App = () => (
     <Router>
         <div>
             <Links />
-            <Route exact path="/" component={Home} />
-            <Route path="/menu" component={Menu} />
-            <Route path="/menu/food" component={Food} />
-            <Route path="/menu/drinks" component={Drinks} />
-            <Route path="/menu/sides" component={Sides} />
+            <Switch>
+                <Route exact path="/" render={() => <h1>Home</h1>}/>
+                <Route path="/new" render={() => <h1>New</h1>}/>
+                <Route path="/old" render={() => <Redirect to="/new" />}/>
+            </Switch>
         </div>
     </Router>
 );
