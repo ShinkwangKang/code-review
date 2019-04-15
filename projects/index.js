374533b9325903c218fec91dfb8b933d50a3dda8
강신광 <shine1115@naver.com>
2019-04-15 16:03:12 +0900

    1-9 예제

--- projects/index.js
+++ projects/index.js
@@ -1,27 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import "./App.css";
+
+const isLoggedIn = false;
 
 const Links = () => (
-    <nav>
-        <div><Link to="/">Home</Link></div>
-        <div><Link to="/old">Old</Link></div>
-        <div><Link to="/new">New</Link></div>
-    </nav>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/protected">인증된 사람만..</Link>
+  </nav>
 );
+
+const SignIn = () => (
+  <div>
+    <input placeholder="ID" type="text" />
+    <input placeholder="PW" type="password" />
+  </div>
+);
+
+const ProtectedPage = () => <h2>인증된 페이지</h2>;
 
 const App = () => (
-    <Router>
-        <div>
-            <Links />
-            <Switch>
-                <Route exact path="/" render={() => <h1>Home</h1>}/>
-                <Route path="/new" render={() => <h1>New</h1>}/>
-                <Route path="/old" render={() => <Redirect to="/new" />}/>
-            </Switch>
-        </div>
-    </Router>
+  <Router>
+    <div>
+      <Links />
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route
+        path="/signin"
+        render={() => (isLoggedIn ? <Redirect to="protected" /> : <SignIn />)}
+      />
+      <Route
+        path="/protected"
+        render={() =>
+          isLoggedIn ? <ProtectedPage /> : <Redirect to="/signin" />
+        }
+      />
+    </div>
+  </Router>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
