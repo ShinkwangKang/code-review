e8a722201b0a85b611eaf41bd5c2c497d87670bf
강신광 <shine1115@naver.com>
2019-04-15 15:11:45 +0900

    1-7 예제 실습

--- projects/index.js
+++ projects/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 
 const Links = () => (
     <nav>
-        <div><Link to="/home">Home</Link></div>
-        <div><Link to="/about">About</Link></div>
+        <div><Link to="/">Home</Link></div>
+        <div><Link to="/menu">Menu</Link></div>
     </nav>
 );
 
-const Header = () => (
-    <div className="header">
-        <Route path="/:page" render={({match}) => <h1>{match.params.page}</h1>} />
-    </div>
-)
-
-const Content = () => (
+const Home = () => <h1>Home</h1>;
+const Food = () => <div>food</div>;
+const Drinks = () => <div>drinks</div>;
+const Sides = () => <div>sides</div>;
+const Menu = () => (
     <div>
-        <Route path="/:page" render={({match}) => <div>{match.params.page} content</div>}/>
+        <div><h1>Menu</h1></div>
+        <div>
+            <nav>
+                <div><Link to="/menu/food">food</Link></div>
+                <div><Link to="/menu/drinks">drinks</Link></div>
+                <div><Link to="/menu/sides">sides</Link></div>
+            </nav>
+        </div>
     </div>
 )
 
@@ -26,8 +31,11 @@ const App = () => (
     <Router>
         <div>
             <Links />
-            <Header />
-            <Content />
+            <Route exact path="/" component={Home} />
+            <Route path="/menu" component={Menu} />
+            <Route path="/menu/food" component={Food} />
+            <Route path="/menu/drinks" component={Drinks} />
+            <Route path="/menu/sides" component={Sides} />
         </div>
     </Router>
 );
