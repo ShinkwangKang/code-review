b55ec0682fdfc50d0d6092507da551167f4a83bd
강신광 <shine1115@naver.com>
2019-04-15 14:56:11 +0900

    11page 연습

--- projects/index.js
+++ projects/index.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import './App.css';
 
 const Links = () => (
     <nav>
-        <Link to="/?id=123">Inline</Link>
-        <Link to={{pathname: '/', search: 'id=456&x=1'}}>Object</Link>
+        <div><Link to="/home">Home</Link></div>
+        <div><Link to="/about">About</Link></div>
     </nav>
 );
+
+const Header = () => (
+    <div className="header">
+        <Route path="/:page" render={({match}) => <h1>{match.params.page}</h1>} />
+    </div>
+)
+
+const Content = () => (
+    <div>
+        <Route path="/:page" render={({match}) => <div>{match.params.page} content</div>}/>
+    </div>
+)
 
 const App = () => (
     <Router>
         <div>
-        React Advanced 실습 2
-            <Links/>
-
-            <Route path="/" 
-                render={({match, location}) => (
-                <div> 
-                    <p>root</p>
-                    <div>{JSON.stringify(match)}</div>
-                    <div>{JSON.stringify(location)}</div>
-                    <div>여기에 매치되는 쿼리 문자열의 id를 출력하세요.</div>
-                </div>
-            )}/>
+            <Links />
+            <Header />
+            <Content />
         </div>
     </Router>
 );
